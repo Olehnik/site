3a92acdd292cbe3c717364025ad11006b05f5bd6
Nikita Olehnik <nikita.oliekhnik@bk.ru>
2015-04-14 00:03:55 +0400

    move

--- 12cvadrat.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(-1);
-header('Content-Type: text/html; charset=UTF-8');
-$kvd = range(1, 17);
-$kvd = array_map(function($num) { return $num * $num; }, $kvd);
-echo '[', join(', ', $kvd), ']';
-?>
--- 1chislo.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(-1);
-header('Content-Type: text/html; charset=UTF-8');
-$val = 2233;
-echo array_sum(str_split($val));
-?>
