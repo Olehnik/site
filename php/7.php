e3de23fc84f57ef7bad7df5fa5805335cdf219a0
Nikita Olehnik <nikita.oliekhnik@bk.ru>
2015-04-20 21:39:07 +0400

    n1

--- php/7.php
+++ php/7.php
@@ -6,24 +6,26 @@
 <body>
 <p>http://php720.com/task/7</p>
 
-<?php if(isset($_POST['gradus']) && $_POST["gradus"] >= 0  && $_POST["gradus"] <= 360){ 
-$gradus = $_POST["gradus"];
-$gradus = (int)$_POST['gradus'];
-				$number = 43200*$gradus/360;	
-				$h   = $number /3600;
+<?php 
+$gradus = 0;
+	if(isset($_POST['gradus'])){
+		$gradus = (int)$_POST['gradus'];
+			if($gradus >= 0  && $gradus <= 360){ 
+				$number = 43200*$gradus/360;	// 43200 - кол-во секунд в 12 часах 
+				$h = $number /3600;
 				$hours = round($h,0);
-				$min = $number % 3600 / 60; 
-				$sec = $number % 3600 % 60;
 				echo "Введено $gradus градусов<br>";
-				echo "Время:<br>"; 
-				echo "$hours : $min : $sec";
-} else { 
+				echo "Часы:<br>"; 
+				echo "$hours";
+			}
+	}
+	else { 
 ?>
 <h1>Введите градусы (0 - 360):</h1>
-<form method="post" action=""> 
-<input type="text" name="gradus"> 
-<input type="submit" value="Отправить" name="submit"> 
-</form>
+	<form method="post" action=""> 
+		<input type="text" name="gradus"> 
+		<input type="submit" value="Отправить" name="submit"> 
+	</form>
 <?php } ?>
 
 </body>
