a42be9a5acf7602351144a61ff69cfb7b1378cc6
Nikita Olehnik <nikita.oliekhnik@bk.ru>
2015-04-22 23:34:27 +0400

    нн

--- php/11.php
+++ php/11.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <head>
-	<meta charset="UTF-8">
-	<title>РљРёС‚Р°Р№СЃРєРёР№ РєР°Р»РµРЅРґР°СЂСЊ</title>
+	<meta charset="1251-winows">
+	<title>Китайский календарь</title>
 </head>
 <body>
 <p>http://php720.com/task/11</p>
-<h1>Р’РІРµРґРёС‚Рµ РіРѕРґ:</h1>
-			<form method="post" action="">           
-            <input type="text" name="year">			
-            <input type="submit" value="РћС‚РїСЂР°РІРёС‚СЊ" name="submit">        
-			</form>
-<?php			
-	$year = $_POST["year"];
-		if($year >= 1924)
-			{				
-				$animal = array("РєСЂС‹СЃС‹","РєРѕСЂРѕРІС‹","С‚РёРіСЂР°","Р·Р°Р№С†Р°","РґСЂР°РєРѕРЅР°","Р·РјРµРё","Р»РѕС€Р°РґРё","РѕРІС†С‹","РѕР±РµР·СЊСЏРЅС‹","РєСѓСЂРёС†С‹","СЃРѕР±Р°РєРё","СЃРІРёРЅСЊРё");
-				echo "Р­С‚Рѕ РіРѕРґ ".$animal[(($year-1984)%12)]."!";
-			}
-		else
-			{
-				echo "Р’С‹ РЅРµ РІРІРµР»Рё РіРѕРґ!";
-			}				
+
+<?php 
+$year = 0;
+
+	if(isset($_POST['year'])){
+		$year = (int)$_POST['year'];
+			if($year >= 1924){ 
+				$animal = array("крысы","коровы","тигра","зайца","дракона","змеи","лошади","овцы","обезьяны","курицы","собаки","свиньи");
+				echo "Это год ".$animal[(($year-1924)%12)]."!";				
+			} 
+			else{						
+					print '<p><font color="red">Данные неккоректны!</font><br>';
+				}			
+	}
+	else { 
 ?>
+<h1>Введите год :</h1>
+	<form method="post" action=""> 
+		<input type="text" name="year"> 
+		<input type="submit" value="Отправить" name="submit"> 
+	</form>
+<?php } ?>
+
 </body>
 </html>
