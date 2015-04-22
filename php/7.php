fe8d097df03785e19d48bf0617fca1a2bf15b54a
Nikita Olehnik <nikita.oliekhnik@bk.ru>
2015-04-22 23:00:23 +0400

    нн

--- php/7.php
+++ php/7.php
@@ -8,16 +8,14 @@
 
 <?php 
 $gradus = 0;
-$hours =0;
+$hours = 0;
 	if(isset($_POST['gradus'])){
 		$gradus = (int)$_POST['gradus'];
-			if($gradus >= 0  && $gradus <= 360){ 
+			if($gradus >= 0 && $gradus <= 360){ 
 				$number = 43200*$gradus/360;	// 43200 - кол-во секунд в 12 часах 
 				$h = $number /3600;
-				$hours = round($h,0);
-				
-				
-			} 
+				$hours = round($h,0);			
+				} 
 			else{	
 					?>
 					<h1>Введите градусы (0 - 360):</h1>
@@ -26,21 +24,21 @@ $hours =0;
 						<input type="submit" value="Отправить" name="submit"> 
 						</form>
 						<?php
-							print '<p><font color="red">Данные неккоректны!</font></p><br>';					
+							print '<h2><font color="red">Данные некоректны!</font></h2>';					
 				}
-				echo "Введено: ". $_POST['gradus']."!<br>";
-				echo "Часы: "; 
-				echo $hours;
-			
+				echo "Введено: ".$_POST['gradus']."!<br>";
+				echo "Часы: ".$hours; 			
 	}
 	else { 
-?>
-<h1>Введите градусы (0 - 360):</h1>
-	<form method="post" action=""> 
-		<input type="text" name="gradus"> 
-		<input type="submit" value="Отправить" name="submit"> 
-	</form>
-<?php } ?>
+		?>
+			<h1>Введите градусы (0 - 360):</h1>
+				<form method="post" action=""> 
+					<input type="text" name="gradus"> 
+					<input type="submit" value="Отправить" name="submit"> 
+				</form>
+		<?php 
+			} 
+				?>
 
 </body>
 </html>
