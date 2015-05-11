9443e556492d1ac81fd8dd9c94f032f904103905
Nikita Olehnik <nikita.oliekhnik@bk.ru>
2015-05-12 01:40:11 +0400

    нн

---
+++ phpblog/blocks/bd.php
@@ -0,0 +1,4 @@
+﻿<?php
+$db = mysql_connect("localhost","bloguser","12345");
+mysql_select_db("phpblog",$db);
+?>
---
+++ phpblog/blocks/footer.php
@@ -0,0 +1,3 @@
+﻿<tr>
+    <td height="31"><img src="img/footer.jpg" width="690" height="20" /></td>
+  </tr>
